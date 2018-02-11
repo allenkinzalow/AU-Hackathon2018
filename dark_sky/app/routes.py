7b3f1e80395dd8ffb18509fdf67a4130862bfee0
= <=>
2018-02-10 19:54:09 -0600

    proxy server

--- dark_sky/app/routes.py
+++ dark_sky/app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template
+from flask import request
 from app import app
+
+import requests
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+#Proxy Server
+@app.route('/forecast/<string:lat>/<string:lng>')
+def forecast(lat, lng):
+    r = requests.get('https://api.darksky.net/forecast/a2194fb69b46af4a08cc6cf40f3b4cc2/'+lat+','+lng)
+    return r.content
